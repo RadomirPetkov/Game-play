4d0245d02e6283094b7fcc6826c4bb6531610d74
Radomir Petkov <105939480+RadomirPetkov@users.noreply.github.com>
2022-07-19 22:03:01 +0300

    Add routes to all pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import './App.css';
 import { Header } from "./components/Header/Header"
 import { Home } from './components/Home/Home';
 import { Routes, Route } from 'react-router-dom'
+import { Login } from './components/Login/Login';
+import { Register } from './components/Register/Register';
+import { CreateGame } from './components/CreateGame/CreateGame';
+import { Catalog } from './components/Cataolog/Catalog';
 
 
 function App() {
@@ -11,7 +15,10 @@ function App() {
       <main id="main-content">
         <Routes>
           <Route path='/' element={<Home />} />
-
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/create' element={<CreateGame />} />
+          <Route path='/catalog' element={<Catalog />} />
         </Routes>
 
 
