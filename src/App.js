53dd1b832d7124298e3b03c3af21a36c75b103a2
Radomir Petkov <105939480+RadomirPetkov@users.noreply.github.com>
2022-07-19 21:49:41 +0300

    add router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import { Header } from "./components/Header/Header"
 import { Home } from './components/Home/Home';
+import { Routes, Route } from 'react-router-dom'
+
 
 function App() {
   return (
     <div className="App">
       <Header />
       <main id="main-content">
-        <Home />
+        <Routes>
+          <Route path='/' element={<Home />} />
 
+        </Routes>
 
 
 
--- src/components/Home/Game/Game.js
+++ src/components/Home/Game/Game.js
@@ -1,12 +1,12 @@
 
 
-export const Game = (props) => {
-      return (
+export const Game = ({ imageUrl, title, _id }) => {
+    return (
         <div className="game">
             <div className="image-wrap">
-                <img src={props.imageUrl} />
+                <img src={imageUrl} />
             </div>
-            <h3>{props.imageUrl}</h3>
+            <h3>{title}</h3>
             <div className="rating">
                 <span>☆</span>
                 <span>☆</span>
@@ -15,7 +15,7 @@ export const Game = (props) => {
                 <span>☆</span>
             </div>
             <div className="data-buttons">
-                <a href={`/${props._id}`} className="btn details-btn">
+                <a href={`/${_id}`} className="btn details-btn">
                     Details
                 </a>
             </div>
